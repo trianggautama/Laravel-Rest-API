c4f69a2033cf7965ae475825d8e2c89578f58800
trianggautama <trianggategarutama@gmail.com>
2022-05-19 07:29:09 +0800

    handle csrf missmatch and resource user

---
+++ app/Http/Resources/V1/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            $this->merge(Arr::accept(parrent::toArray($request),[
+                'created_at','updated_at','email','email_verified_at'
+            ]))
+        ]
+    }
+}
